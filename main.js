f3380e779b33dd821f8bd36e7465cfcfb5c4383b
Evan Supple <evansupple@pop-os.localdomain>
2021-05-23 14:41:28 -0400

    fixed double click problem on switch

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 const marriedRadio= document.getElementById('gridRadios1');
+const divorcedRadio= document.getElementById('gridRadios2');
+const widowedRadio= document.getElementById('gridRadios3');
+const nevermarriedRadio= document.getElementById('gridRadios4');
+
 const form = document.getElementById('no.2');
 const fieldset=document.getElementById('marriedRadio');
 const myradios= document.getElementById('myradios');
 let allow = 'y'	
-function choicemade(){
 
 
-if(marriedRadio.checked&& allow=='y'){
-const para = document.createElement('p');
-const spouseInput=document.createElement('input');
+
+function switchon(){
+if(allow=='y'){
+ para = document.createElement('p');
+spouseInput=document.createElement('input');
 spouseInput.setAttribute("type", "text");
 spouseInput.setAttribute("Id", "newspouse");
 spouseInput.setAttribute("class", "col-md-6");
@@ -16,30 +21,79 @@ para.textContent = ' Name of Spouse:';
 fieldset.appendChild(para);
 fieldset.appendChild(spouseInput);
   allow='n';
-}
-else{ 
 
-	para2=document.querySelector('p');
-	para2.remove();
-	spouseInput2=document.getElementById('newspouse');
-	spouseInput2.remove();
+}
+
+}
+
+function switchoff(){
+	if(allow='n'){
+	para.remove();
+	spouseInput.remove();
 	 allow='y';
 
+
+	}
+
+
 }
-//set it to do nothing if its pressed again
+
+
+marriedRadio.addEventListener('click', switchon);
+divorcedRadio.addEventListener('click', switchoff);
+widowedRadio.addEventListener('click', switchoff )
+nevermarriedRadio.addEventListener('click', switchoff)
+
+
+
+//functionize this
+//parameters:   no of total targets, pos of positive target,  func_if_posiitive, func_if_negative, targets,
+//this should really be a class 
+
+
+//just create a class for yes no questiond with a consturctor,
+//yesno class is gonna be constructed with what dom stuff it will create when switched on
+//yesno(yes DOM obj, no Dom obj, label to be displayed when yes is clicked along wit input)
+
+
+function Yesno(yesDom, noDom, inputLabel){
+this.yesDom=yesDom;
+this.noDom=noDom;
+this.inputLabel=inputLabel;
+}
+
+
+
+Yesno.prototype.execute = function(){
+
+	function yesScript(){
+		if(allow=='y'){
+ para = document.createElement('p');
+spouseInput=document.createElement('input');
+spouseInput.setAttribute("type", "text");
+spouseInput.setAttribute("Id", "newspouse");
+spouseInput.setAttribute("class", "col-md-6");
+para.textContent = ' Name of Spouse:';
+fieldset.appendChild(para);
+fieldset.appendChild(spouseInput);
+  allow='n';
 
 }
 
-marriedRadio.addEventListener('click', choicemade);
+
+
+	}
+
+	function noScript(){
+
+	}
 
 
 
 
-//make a function that does something when something is clicked
-//prevent double clicking
-//whenever a different option is clicked that thing that was created goes away
+yesDom.addEventListener('click',  )
+noDom.addEventListener('click',)
+}
 
 
-//how to do this?
-//add event listerner for each positive and negative choice
-//prevent double clicking, prevent negative from
+
